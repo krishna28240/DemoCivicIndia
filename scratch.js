aa5dad3e9d156b4a7b93d38f7c57b1b704588162
Advik Sharma <advik@adviks-mbp.home>
2026-05-01 19:28:41 -0400

    Remove temporary scratch file

--- scratch.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const appJsContent = fs.readFileSync('./public/app.js', 'utf8');
-const match = appJsContent.match(/function createHomeOverview\(\) \{([\s\S]*?)return section;/);
-console.log(match[1]);
